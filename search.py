3bf5a621a6d493dbba2213111d922f44b3dd52b6
Phone Myat Kyaw <pmyatkyaw2018@gmail.com>
2022-03-25 00:31:34 +0630

    Define all the functions in the project

--- search.py
+++ search.py
@@ -1,1 +1,16 @@
 #This is the main function.
+def find_nutrients():
+    pass
+    #  let it return nutrients for the databasee
+def find_price():
+    pass
+    # let it return price for the database
+def insert_database():
+    pass
+    # let it store in a clear database
+def check_products_in_database():
+    pass
+    # check if the function is in database or not
+def display_from_database():
+    pass
+    #WE will display everything from database here.
